871fad8a78cfb2a1a5625b57c33ca3a48691d8f7
FelipeBrocca <pipebrocca@gmail.com>
2022-05-03 17:44:15 -0300

    Archivo secciones

--- SmartMarket/app.js
+++ SmartMarket/app.js
@@ -1,12 +1,39 @@
 const express = require('express');
-
 const app = express();
 const path = require('path');
 
-app.use(express.static('public'));
+const publicPath = path.resolve(__dirname, './public')
+app.use(express.static(publicPath))
 
-app.listen(3000, () => {
-    console.log('corriendo en el puerto 3000');
+app.get('/categories', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, './views/categories.html'))
+});
+app.get('/all-products', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, './views/all-products.html'))
+});
+app.get('/bebidas', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, './views/bebidas.html'))
+});
+app.get('/bebidas-con-alcohol', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, './views/bebidas-con-alcohol.html'))
+});
+app.get('/almacen', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, './views/almacen.html'))
+});
+app.get('/lacteos', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, './views/lacteos.html'))
+});
+app.get('/limpieza', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, './views/limpieza.html'))
+});
+app.get('/higiene', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, './views/higiene.html'))
+});
+app.get('/verduleria', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, './views/verduleria.html'))
+});
+app.get('/carnes', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, './views/carnes.html'))
 });
 
 app.get('/', (req, res) => {
@@ -26,4 +53,7 @@ app.get('/productCart', (req, res) => {
 
 app.get('/productDetail', (req, res) => {
     res.sendFile(path.join(__dirname,'./views/productDetail.html'))
+});
+app.listen(3000, () => {
+    console.log('Servidor corriendo');
 });
