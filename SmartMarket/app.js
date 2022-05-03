77d7bc7328837c48adfbdfadb4484814f079afe9
lenislipari <lenislipari@gmail.com>
2022-05-03 17:29:43 -0300

    Archivos home, register y login

--- SmartMarket/app.js
+++ SmartMarket/app.js
@@ -1,12 +1,25 @@
 const express = require('express');
-const path = require('path');
-const app = express();
 
-const publicPath= path.resolve(__dirname, './public');
+const app = express();
+const path = require('path');
+
 app.use(express.static('public'));
 
-app.listen(3000, () => console.log('servidor corriendo en el puerto 3000'))
+app.listen(3000, () => {
+    console.log('corriendo en el puerto 3000');
+});
 
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views/home.html'));
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views/register.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views/login.html'));
+});
 app.get('/productCart', (req, res) => {
     res.sendFile(path.join(__dirname,'./views/productCart.html'))
 });
