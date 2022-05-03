04c5a7d7368b590ec44736b7bab073a15bb341b1
Lilia Padilla <lilopadilla22@gmail.com>
2022-05-03 16:17:50 -0500

    Borrar documento sobrante, arreglos del app.js

--- SmartMarket/app.js
+++ SmartMarket/app.js
@@ -5,6 +5,10 @@ const path = require('path');
 const publicPath = path.resolve(__dirname, './public')
 app.use(express.static(publicPath))
 
+app.listen(3000, () => {
+    console.log('Servidor corriendo');
+});
+
 app.get('/categories', (req, res) =>{
     res.sendFile(path.resolve(__dirname, './views/categories.html'))
 });
@@ -53,7 +57,4 @@ app.get('/productCart', (req, res) => {
 
 app.get('/productDetail', (req, res) => {
     res.sendFile(path.join(__dirname,'./views/productDetail.html'))
-});
-app.listen(3000, () => {
-    console.log('Servidor corriendo');
 });
