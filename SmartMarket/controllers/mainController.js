214d4a5c8f31ba2b8f72affc4ad64c89d58d9f0d
FelipeBrocca <pipebrocca@gmail.com>
2022-05-13 16:57:05 -0300

    Arreglo controladores

--- SmartMarket/controllers/mainController.js
+++ SmartMarket/controllers/mainController.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-// const bebidasConAlcohol = require('bebidasConAlcohol');
+const bebidasConAlcohol = require('./productsController');
 
 const mainController = {
 
@@ -52,78 +52,7 @@ const mainController = {
     
 };
 
-let bebidasConAlcohol = [
-    {
-        id: '00',
-        imagen: 'schneider 150.jpg',
-        nombre:'Schneider',
-        detalle: 'Cerveza Schneider 1lt. Lager',
-        precio: '$136'
-    },
-    {
-        id: '01', 
-        imagen: 'heineken 340.jpg',
-        nombre:'Heineken',
-        detalle: 'Cerveza Heineken 1lt. Lager',
-        precio: '$327',
-    },
-    {
-        id: '02', 
-        imagen: 'stella 380.jpg',
-        nombre:'Stella Artois',
-        detalle: 'Cerveza Stella Artois 1lt. Lager',
-        precio: '$365',
-    },
-    {
-        id: '03', 
-        imagen: 'saint felic malbec 750 cc 1890.jpg',
-        nombre:'Saint Felicien',
-        detalle: 'Vino tinto, malbec. Saint Felicien 750cc',
-        precio: '$1754',
-    },
-    {
-        id: '04', 
-        imagen: 'portillo blanco 750cc 530 chardonnay.jpg',
-        nombre:'Portillo',
-        detalle: 'Vino blanco, chardonnay. Portillo 750cc',
-        precio: '$515',
-    },
-    {
-        id: '05', 
-        imagen: 'fernet 750cc 970.jpg',
-        nombre:'Fernet Branca',
-        detalle: 'Fernet Branca 750cc',
-        precio: '$869',
-    },
-    {
-        id: '06', 
-        imagen: 'gin 750cc 3050.jpg',
-        nombre:'Bombay Sapphire',
-        detalle: 'Gin Bombay Sapphire 750cc',
-        precio: '$3015',
-    },
-    {
-        id: '07', 
-        imagen: 'tanqueray 700cc 2900.jpg',
-        nombre:'Tanqueray',
-        detalle: 'Gin Tanqueray 700cc',
-        precio: '$2783',
-    },
-    {
-        id: '08', 
-        imagen: 'corona 710cc 365.jpg',
-        nombre:'Corona',
-        detalle: 'Cerveza Corona 710cc',
-        precio: '$345',
-    },
-    {
-        id: '09', 
-        imagen: 'ron oro 750cc 1300.jpg',
-        nombre:'Bacardí',
-        detalle: 'Ron Bacardí Oro 750cc',
-        precio: '$1239',
-    }
-];
+
 
 
 module.exports = mainController;
