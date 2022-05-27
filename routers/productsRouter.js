0cc39e5d4b21a3c3db8f60309e52896ae7c7ab82
lenislipari <lenislipari@gmail.com>
2022-05-27 16:29:38 -0300

    instalando multer

--- routers/productsRouter.js
+++ routers/productsRouter.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const path = require ('path')
+const multer = require('multer');
 
 const productsController = require('../controllers/productsController');
+
+// ************ MULTER ************
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'public/images/products')
+    },
+
+    filename: function (req, file, cb) {
+    //   
+      cb(null, file.fieldname + 'img' + path.extname(file.originalname))
+    }
+  })
+  
+  const upload = multer({ storage: storage })
 
 
 router.get('/', productsController.index);
@@ -10,6 +26,10 @@ router.get('/productCart', productsController.productCart);
 router.get('/productDetail/:id', productsController.productDetail);
 // router.get('/categories', productsController.categories);
 router.get('/:categoria', productsController.categoriaProducto);
+
+// /*** EDIT ONE PRODUCT ***/ 
+router.get('/edit/:id', productsController.edit); 
+router.put('/edit/:id', upload.any(), productsController.update); 
 
 
 
