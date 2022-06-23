798e1836dd4e6c2c941b6774352798ba29964930
lenislipari <lenislipari@gmail.com>
2022-06-23 17:22:38 -0300

    cambios agrego multer a routerusers

--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -1,7 +1,22 @@
 const express = require('express');
+const multer = require('multer');
 const router = express.Router();
+
 // const path = require ('path');
 const usersController = require ('../controllers/usersController');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'public/img')
+    },
+
+    filename: function (req, file, cb) {
+    //   
+      cb(null, file.fieldname + path.extname(file.originalname))
+    }
+  })
+  
+  const upload = multer({ storage: storage })
 
 
 router.get('/login', usersController.login);
