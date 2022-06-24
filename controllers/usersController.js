d9e4d3da6f0929aa84e2462477c5b6a11f848514
Lilia Padilla <lilopadilla22@gmail.com>
2022-06-24 15:05:58 -0500

    validacion contraseña

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,39 +1,41 @@
 const path = require('path');
 const fs = require('fs')
-const { validationResult } = require('express-validator')
+const {
+    validationResult
+} = require('express-validator')
 
 
 
 const usersController = {
-    
-    login: (req, res) =>{
+
+    login: (req, res) => {
         res.render('login');
     },
-    registro: (req, res) =>{
+    registro: (req, res) => {
         res.render('register');
     },
     nuevoRegistro: (req, res) => {
-     
-    const resultValidation = validationResult(req);
-    
 
-    if(resultValidation.errors.length > 0) {
-         return res.render('register', {
-            errors: resultValidation.mapped()
-         });
-    }
-    return res.send("Funciona el formulario");
+        const resultValidation = validationResult(req);
 
-     
-    //     let newRegister = {
-    //     id: users[users.length - 1].id + 1,
-    //     ...req.body
-    // }
+        console.log(resultValidation);
+        if (resultValidation.errors.length > 0) {
+            return res.render('register', {
+                errors: resultValidation.mapped()
+            });
+        }
+        return res.send("Funciona el formulario");
 
-    // users.push(newRegister)
 
-    // fs.writeFileSync(usersFilePath, JSON.stringify(users));
-    // res.redirect('/')
+        //     let newRegister = {
+        //     id: users[users.length - 1].id + 1,
+        //     ...req.body
+        // }
+
+        // users.push(newRegister)
+
+        // fs.writeFileSync(usersFilePath, JSON.stringify(users));
+        // res.redirect('/')
     }
 }
 
--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -1,49 +1,66 @@
 const express = require('express');
 const multer = require('multer');
 const router = express.Router();
-const path = require ('path');
-const usersController = require ('../controllers/usersController');
-const {body} = require ('express-validator');
+const path = require('path');
+const usersController = require('../controllers/usersController');
+const {
+  body
+} = require('express-validator');
 
 
 
 const validations = [
   body('nombreYApellido').notEmpty().withMessage('Debe agregar nombre y apellido'),
   body('usuario').notEmpty().withMessage('Debe agregar un nombre de usuario'),
-  body('password').notEmpty().withMessage('Debe ingresar una contraseña'),
-  body('repeat-password').equals('repeat-password').withMessage('La contraseña debe coincidir'),
+  body('password').notEmpty().withMessage('Debe ingresar una contraseña').bail()
+  .custom((value, {
+    req
+  }) => {
+    console.log(req.body);
+    let contraseña = req.body.password;
+    let repeat = req.body["repeat-password"];
+
+    if (contraseña != repeat) {
+      throw new error('Las contraseñas deben coincidir')
+    }
+    return true;
+  }),
   body('pais').notEmpty().withMessage('Debe agregar un país'),
   body('direccion').notEmpty().withMessage('Debe agregar una dirección'),
   body('terminos').notEmpty().withMessage('Debe aceptar los términos y condiciones'),
-  body('fotoPerfil').custom((value, {req}) => {
+  body('fotoPerfil').custom((value, {
+    req
+  }) => {
     let file = req.file;
-    let acceptedExtensions = ['.jpg','.JPEG','.JPG', '.PNG', '.png', '.gif'];
-   
-    if(!file) {
+    let acceptedExtensions = ['.jpg', '.JPEG', '.JPG', '.PNG', '.png', '.gif'];
+
+    if (!file) {
       throw new Error('Debes subir una imágen')
     } else {
       let fileExtension = path.extname(file.originalname);
-    if(!acceptedExtensions.includes(fileExtension)){
-      throw new Error(`Las extensiones de archivo permitidas son ${acceptedExtensions.join(', ')}`)
+      if (!acceptedExtensions.includes(fileExtension)) {
+        throw new Error(`Las extensiones de archivo permitidas son ${acceptedExtensions.join(', ')}`)
+      }
     }
-  }
     return true;
   })
 
 ]
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'public/images/users')
-    },
+  destination: function (req, file, cb) {
+    cb(null, 'public/images/users')
+  },
 
-    filename: function (req, file, cb) {
+  filename: function (req, file, cb) {
     //   
-      cb(null, file.fieldname + path.extname(file.originalname))
-    }
-  })
-  
-  const uploadFile = multer({ storage: storage })
+    cb(null, file.fieldname + path.extname(file.originalname))
+  }
+})
+
+const uploadFile = multer({
+  storage: storage
+})
 
 
 router.get('/login', usersController.login);
