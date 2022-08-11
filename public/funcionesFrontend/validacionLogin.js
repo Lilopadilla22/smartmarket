8f0d8e71207c0377d38e024660e545033e834b76
lenislipari <lenislipari@gmail.com>
2022-08-11 17:02:02 -0300

    validacion front login

---
+++ public/funcionesFrontend/validacionLogin.js
@@ -0,0 +1,23 @@
+window.onload = function(){
+    
+    let smallEmailLogin = document.querySelector(".smallEmailLogin")
+    let emailLogin = document.querySelector('#email');
+         emailLogin.addEventListener("blur", ()=> {
+            if(emailLogin.value == ""){
+                smallEmailLogin.innerHTML = "Ingrese un email"
+            }else if (emailLogin.value.length < 2) {
+                smallEmailLogin.innerHTML = "Ingrese un email"
+            }
+            })
+        
+
+            let smallPasswordLogin = document.querySelector(".smallPasswordLogin")
+    let passwordLogin = document.querySelector('#password');
+    passwordLogin.addEventListener("blur", ()=>{
+        if(passwordLogin == ""){
+            smallPasswordLogin.innerHTML = "Ingrese una contraseña"
+        } else if (passwordLogin.value.length < 1) {
+            smallPasswordLogin.innerHTML = "Ingrese una contraseña"
+        }
+    })
+}
